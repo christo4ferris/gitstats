3a19db8b42c42ef67cbba42533004ba00be1ab21
Christopher B. Ferris <chrisfer@us.ibm.com>
2015-10-26 11:52:03 -0400

    more refactoring
    even more refactoring

--- test/throttle-spec.js
+++ test/throttle-spec.js
@@ -1,6 +1,28 @@
-var app = require('../throttle.js');
+'use strict';
+
+const Throttler = require('../throttle');
+
+var config = {
+	'auth': {
+	'clientid': 'foobar',
+	'secret': 'itsasecret-shhhh'
+	},
+	'db': {
+	'name': 'gitstats-a',
+	'url': 'http://localhost:5984/'
+	},
+	'orgsfile': './orgs.json',
+	'collect_pull_requests': false,
+	'interval': 1000
+};
 
 describe("throttle", function() {
-	it('should ', function() {
+	it('should default', function() {
+		var t = new Throttler();
+		expect(t.getIntervalLength() === 2000).toBe(true);
+	});
+	it('should initialize with config file', function() {
+		var t = new Throttler(config);
+		expect(t.getIntervalLength() === 1000).toBe(true);
 	});
 });
