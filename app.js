3619a74ad71bc5c1abeb061821c49777cdc404f5
Jacques Perrault <jacques_perrault@us.ibm.com>
2015-10-27 09:12:09 -0400

    added promises for last polled, backed out throttle changes

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 /*eslint-env node */
-//var https = require('https');
-var http = require('http');
-var events = require('events');
-var has = require('./src/has');
-var clone = require('./src/clone');
-const Throttler = require('./src/throttle');
-var parse_link = require('./src/parse_link');
-var eventEmitter = new events.EventEmitter();
+var http            = require('http');
+var events          = require('events');
+var promise         = require('promise');
+var has             = require('./src/has');
+var clone           = require('./src/clone');
+var Throttler       = require('./src/throttle');
+var parse_link      = require('./src/parse_link');
+var handle_response = require('./src/handle_response');
+var config          = require('./config');
+var orgs            = require(config.orgsfile);
 
-var config = require('./config');
-var orgs = require(config.orgsfile);
+var eventEmitter    = new events.EventEmitter();
 
-//var port = config.port || 3000;
-//var host = config.host || 'localhost';
+//var port          = config.port || 3000;
+//var host          = config.host || 'localhost';
 
-var org = '';
-var repo = '';
-var token = '';
-var T = new Throttler(config);
+var T               = new Throttler(config);
 
 //require(path.join(__dirname, 'routes.js'))(app); // load our routes and pass in our app, config, and fully configured passport
 
@@ -41,7 +39,7 @@ options.headers['User-Agent'] = config.auth.clientid;
 //options.headers.Authorization = new String('token ' + config.auth.secret);
 
 // Use this for a registered app token
-token = '&client_id=' + config.auth.clientid + '&client_secret=' + config.auth.secret;
+var token = '&client_id=' + config.auth.clientid + '&client_secret=' + config.auth.secret;
 
 Date.prototype.getWeekNo = function(){
 	var d = new Date(+this);
@@ -50,20 +48,26 @@ Date.prototype.getWeekNo = function(){
 	return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
 };
 
-// This is basically an empty shell function
-// It could be used for document pre- or post- handling
-function insertdb(response) {
-	//console.log('--- INSERTDB: statusCode: ', response.statusCode);
-	response.on('error', function(e) {
-		console.log('--- INSERT_DB: ')
-		console.error(e);
-	});
-	response.on('end', function() {
-		if (response.statusCode === 409) {
-			console.log('--- INSERT_DB: document already exists.');
-		}
-	});
-}
+var https = require('https');
+var timer = null;
+var stack = [];
+
+function process() {
+   var item = stack.shift();
+   console.log("process: " + item.opts.path);
+   https.request(item.opts, item.func).end();
+   if (stack.length === 0) {
+       clearInterval(timer);
+       timer = null;
+   };
+ };
+
+function throttle(item) {
+    stack.push(item);
+    if (timer === null) {
+      timer = setInterval(process, config.interval);
+    };
+};
 
 // this will process the link header (if present) and invoke the requested function if a next header is present
 function get_more(response, func) {
@@ -74,7 +78,8 @@ function get_more(response, func) {
 			t.func = func;
 			t.opts = clone(options);
 			t.opts.path = links['next'].substring(22, links['next'].length);
-			T.throttle(t);
+			//T.throttle(t);
+            throttle(t);
 			console.log(response.headers.link);
 			console.log(func.name + ': ' + t.opts.path);
 		}
@@ -115,7 +120,7 @@ function get_pull_requests(response) {
 				var date = new Date(doc.date);
 				doc.week = date.getWeekNo();
 				doc.url = item.url;
-				var db = http.request(optionsdb, insertdb);
+				var db = http.request(optionsdb, handle_response);
 				db.write(JSON.stringify(doc));
 				db.end();
 				// account for pairing situations
@@ -123,7 +128,7 @@ function get_pull_requests(response) {
 				if (has(item.commit, 'author') && item.commit.committer.name != item.commit.author.name) {
 					doc.name = item.commit.author.name;
 					doc.email = item.commit.author.email;
-					db = http.request(optionsdb, insertdb);
+					db = http.request(optionsdb, handle_response);
 					db.write(JSON.stringify(doc));
 					db.end();
 				}
@@ -172,14 +177,14 @@ function get_commits(response) {
 					date.setDate(doc.date);
 					doc.week = date.getWeekNo();
 					doc.url = item.url;
-					var db = http.request(optionsdb, insertdb);
+					var db = http.request(optionsdb, handle_response);
 					db.write(JSON.stringify(doc));
 					db.end();
 					// account for pairing situations
 					if (has(item.commit, 'author') && item.commit.committer.name != item.commit.author.name) {
 						doc.name = item.commit.author.name;
 						doc.email = item.commit.author.email;
-						db = http.request(optionsdb, insertdb);
+						db = http.request(optionsdb, handle_response);
 						db.write(JSON.stringify(doc));
 						db.end();
 					}
@@ -217,14 +222,16 @@ function get_repos(response) {
 			// get commits
 			t.func = get_commits;
 			t.opts.path = '/repos/' + r[0] + '/' + r[1] + '/commits?per_page=100'  + token;
-			T.throttle(t);
+			//T.throttle(t);
+            throttle(t);
 			//console.log('--- GET_REPOS: get_commits: ' + t.opts.path);
 
 			// get pull requests
 			if (config.collect_pull_requests) {
 				t.func = get_pull_requests;
 				t.opts.path = '/repos/' + r[0] + '/' + r[1] + '/pulls?per_page=100&state=all'  + token;
-				T.throttle(t);
+				//T.throttle(t);
+                throttle(t);
 			}
 			//console.log('--- GET REPOS: get_pull_requests: ' + t.opts.path);
 		});
@@ -286,6 +293,43 @@ function create_db() {
 	}).end();
 }
 
+
+function get_lastpolled(repo) {
+    var deferred = new Promise(function(resolve, reject) {
+        var doc = {};
+        var opts = clone(optionsdb);
+        opts.method = 'GET';
+        opts.path = '/' + config.db.name + '/' + repo.replace(/\//g, '---');
+        var result = '2016-01-01T00:00:00Z'; // default to earliest UTC value
+
+        var req = http.request(opts, function(res){
+            res.on('data', function(chunk) {
+                if ((res.statusCode === 200) || (res.statusCode === 304)) {
+                    doc = JSON.parse(chunk);
+                    //console.log('--- GET_LASTPOLLED: old date: ',doc.repofullname, doc.date);
+                    result = doc.date;
+
+                } else {
+                    // create the lastpolled document
+                    //console.log('--- GET_LASTPOLLED: creating: ',repo);
+                    doc.type = 'lastpolled';
+                    doc.repofullname = repo;
+                }
+
+                // update / create lastpolled doc
+                opts.method = 'PUT'
+                doc.date = new Date().toISOString();
+                var db = http.request(opts, handle_response);
+                db.write(JSON.stringify(doc))
+                db.end();
+                //console.log('--- GET_LASTPOLLED: new date: ',doc.repofullname, doc.date);
+                resolve(result);
+            })
+        }).end();
+    });
+    return deferred; // <--- happens IMMEDIATELY (object that promise listens on)
+}
+
 function load_orgs() {
 	if (orgs.length >= 1) {
 		orgs.forEach(function(item) {
@@ -297,38 +341,40 @@ function load_orgs() {
 			// if this is an 'org' or 'user', put it in the queue to enumerate into a repo
 			if((item.type == 'org') || (item.type == 'user'))  {
 				t.func = get_repos;
-				org = item.name;
+				var org = item.name;
 				t.opts = clone(options);
 				t.opts.path = '/' + item.type + 's/' + org + '/repos?per_page=100' + token;
-				T.throttle(t);
+				//T.throttle(t);
+                throttle(t);
 			}
 
 			// if this is a repo, queue up requests for commits and pull_requests
 			else if (item.type == 'repo') {
-				repo = item.name;
+				var repo = item.name;
 
 				// create or update the 'last polled' pointer for each repo
-				var doc = {};
-				optionsdb.path = '/' + config.db.name + '/' + item.name.replace(/\//g, '---');
-				doc.type = 'lastpolled';
-				doc.repofullname = item.name;
-				doc.date = new Date();
-				var db = http.request(optionsdb, insertdb);
-				db.write(JSON.stringify(doc));
-				db.end();
-				console.log('--- LOAD_ORGS: repo: ' + optionsdb.path);
+                Promise.resolve(get_lastpolled(repo)
+                    .then (function (result) {
+                        var since = '&since=' + result;
+                        //console.log('--- LOAD_ORGS: ' + optionsdb.path, result);
 
-				// get commits
-				t.func = get_commits;
-				t.opts.path = '/repos/' + repo + '/commits?per_page=100' + token;
-				T.throttle(t);
-				//console.log('--- LOAD ORGS: get_commits: ' + t.opts.path);
+                        // get commits
+                        t.func = get_commits;
+                        t.opts.path = '/repos/' + repo + '/commits?per_page=100' + since + token;
+                        //T.throttle(t);
+                        throttle(t);
+                        //console.log('--- LOAD ORGS: get_commits: ' + t.opts.path);
 
-				// get pull requests
-				s.func = get_pull_requests;
-				s.opts.path = '/repos/' + repo + '/pulls?per_page=100&state=all' + token;
-				T.throttle(s);
-				//console.log('--- LOAD ORGS: get_pull_requests: ' + s.opts.path);
+                        // get pull requests
+                        s.func = get_pull_requests;
+                        s.opts.path = '/repos/' + repo + '/pulls?per_page=100&state=all' + since + token;
+                        //T.throttle(s);
+                        throttle(t);
+                        //console.log('--- LOAD ORGS: get_pull_requests: ' + s.opts.path);
+                    })
+                    .catch(function (reason) {
+                    })
+                );
 			}
 		});
 	}
